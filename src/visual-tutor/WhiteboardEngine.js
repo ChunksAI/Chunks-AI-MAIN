e46c9970b8833d76de682423b8573f9011cdbd8d
Chunks AI <chunks.ai.app@gmail.com>
2026-03-16 18:38:35 +0800

    Update WhiteboardEngine.js

--- src/visual-tutor/WhiteboardEngine.js
+++ src/visual-tutor/WhiteboardEngine.js
@@ -778,5 +778,3 @@ function hexToRgb(hex) {
   const r = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return r ? `${parseInt(r[1],16)},${parseInt(r[2],16)},${parseInt(r[3],16)}` : '200,214,229';
 }
-
-export { WhiteboardEngine };
